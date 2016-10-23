a6f895b5e524612db82f432d892bdd188fe0ef94
NicoPaez <nicopaez@computer.org>
2016-10-22 22:01:45 -0300

    Package info updated

--- System.IO.Utilities/Properties/AssemblyInfo.cs
+++ System.IO.Utilities/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("System.Extensions.IO")]
+[assembly: AssemblyTitle("System.IO.Utilities")]
 [assembly: AssemblyDescription("Wrapper library over System.IO.File class")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NicoPaez")]
-[assembly: AssemblyProduct("System.Utilities")]
+[assembly: AssemblyProduct("System.IO.Utilities")]
 [assembly: AssemblyCopyright("Copyright © NicoPaez 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
